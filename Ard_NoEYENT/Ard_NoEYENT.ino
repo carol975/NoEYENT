4d950ad530cad1dbfd31829dfa2302ba8406077e
Carol Fu <carolf@alumni.ubc.ca>
2018-03-03 21:37:29 -0800

    fix buffer overflow in random index

--- Ard_NoEYENT/Ard_NoEYENT.ino
+++ Ard_NoEYENT/Ard_NoEYENT.ino
@@ -1,11 +1,18 @@
-//response button pin 
-#define rsp_but 9;
+#define baud_rate 9600
 
+//response button pin 
+const byte rsp_but = 2;
+volatile byte rsp_clicked = LOW;
+ 
 //define LED pin 
-int LEDS[] = {1,2,3,4,5,6,7,8};
+int LEDS[] = {3,4,5,6,7,8,9,10};
 
 void setup() {
-  pinMode(rsp_but, INPUT);
+  Serial.begin(baud_rate);
+  //Setup for interrupts
+  pinMode(rsp_but, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(rsp_but), click, HIGH);
+
   
   for(int LED_NUM = 0; LED_NUM < sizeof(LEDS); LED_NUM++) {
     pinMode(LEDS[LED_NUM], OUTPUT);
@@ -13,7 +20,7 @@ void setup() {
 
   //shuffle LEDS ord
   for (int i = 0; i < sizeof(LEDS); i++) {
-    int r = random(i, 8);
+    int r = random(i, 7);
     int temp = LEDS[i];
     LEDS[i] = LEDS[r];
     LEDS[r] = temp;
@@ -22,8 +29,9 @@ void setup() {
 }
 
 void loop() {
-  for(int LED_NUM = 0; LED_NUM < sizeof(LEDS); LED_NUM++) {
+  for(int LED_NUM = 0; LED_NUM < 8; LED_NUM++) {
     digitalWrite(LEDS[LED_NUM],HIGH);
+    Serial.println(LEDS[LED_NUM]);
     delay(500);
     digitalWrite(LEDS[LED_NUM],LOW);
     int r_delay = random(200, 2000);
@@ -31,3 +39,11 @@ void loop() {
   }
 
 }
+
+void click() {
+  rsp_clicked = HIGH;
+  Serial.println("rsp_clicked");
+  rsp_clicked = LOW; //these are just for later use maybe..
+}
+
+
