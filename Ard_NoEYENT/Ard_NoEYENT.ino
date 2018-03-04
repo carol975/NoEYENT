061d227f5293ea176a648eeada5d2eb9098f0843
Carol Fu <carolf@alumni.ubc.ca>
2018-03-03 20:44:10 -0800

    random delay and random sequence of LED

--- Ard_NoEYENT/Ard_NoEYENT.ino
+++ Ard_NoEYENT/Ard_NoEYENT.ino
@@ -1,21 +1,33 @@
-//define LED pin 
-int LEDS[] = {0,1,2,3,4,5,6,7,8};
+//response button pin 
+#define rsp_but 9;
 
+//define LED pin 
+int LEDS[] = {1,2,3,4,5,6,7,8};
 
 void setup() {
-  for(int LED_NUM = 1; LED_NUM < sizeof(LEDS); LED_NUM++) {
+  pinMode(rsp_but, INPUT);
+  
+  for(int LED_NUM = 0; LED_NUM < sizeof(LEDS); LED_NUM++) {
     pinMode(LEDS[LED_NUM], OUTPUT);
   }
 
+  //shuffle LEDS ord
+  for (int i = 0; i < sizeof(LEDS); i++) {
+    int r = random(i, 8);
+    int temp = LEDS[i];
+    LEDS[i] = LEDS[r];
+    LEDS[r] = temp;
+  }
 
 }
 
 void loop() {
-  for(int LED_NUM = 1; LED_NUM < sizeof(LEDS); LED_NUM++) {
+  for(int LED_NUM = 0; LED_NUM < sizeof(LEDS); LED_NUM++) {
     digitalWrite(LEDS[LED_NUM],HIGH);
     delay(500);
     digitalWrite(LEDS[LED_NUM],LOW);
-    delay(500);
+    int r_delay = random(200, 2000);
+    delay(r_delay);
   }
 
 }
