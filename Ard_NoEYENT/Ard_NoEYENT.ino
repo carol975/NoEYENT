eb70ddba32db00e8e6e763f160abddfd90fe7288
Carol Fu <carolf@alumni.ubc.ca>
2018-03-03 20:30:40 -0800

    replace pin 0 with pin 8

--- Ard_NoEYENT/Ard_NoEYENT.ino
+++ Ard_NoEYENT/Ard_NoEYENT.ino
@@ -1,9 +1,9 @@
 //define LED pin 
-int LEDS[] = {0,1,2,3,4,5,6,7};
+int LEDS[] = {0,1,2,3,4,5,6,7,8};
 
 
 void setup() {
-  for(int LED_NUM = 0; LED_NUM < sizeof(LEDS); LED_NUM++) {
+  for(int LED_NUM = 1; LED_NUM < sizeof(LEDS); LED_NUM++) {
     pinMode(LEDS[LED_NUM], OUTPUT);
   }
 
@@ -11,7 +11,7 @@ void setup() {
 }
 
 void loop() {
-  for(int LED_NUM = 0; LED_NUM < sizeof(LEDS); LED_NUM++) {
+  for(int LED_NUM = 1; LED_NUM < sizeof(LEDS); LED_NUM++) {
     digitalWrite(LEDS[LED_NUM],HIGH);
     delay(500);
     digitalWrite(LEDS[LED_NUM],LOW);
